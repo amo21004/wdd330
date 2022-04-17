b5b81443d63a21d0082066dd4abe8a1e1775041b
amo21004 <amo21004@byui.edu>
2022-04-17 15:07:18 +0530

    First commit

---
+++ js/portfolio.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const toc_url = "json/toc.json";
+  
+    fetch(toc_url)
+      .then((response) => response.json())
+      .then((json_object) => {
+        const toc_list = document.querySelector("#toc");
+    
+        for (temple of json_object.links) {
+          const li_element = document.createElement("li");
+  
+          const anchor_element = document.createElement('a');
+
+          anchor_element.href = temple.link;
+
+          anchor_element.innerText = temple.label;
+
+          li_element.appendChild(anchor_element);
+
+          toc_list.appendChild(li_element);
+
+        }
+      });
+  });
+  
