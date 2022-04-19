c54a58a6869b60e7c838b78ed600019017005ef8
amo21004 <amo21004@byui.edu>
2022-04-19 20:14:06 +0530

    Recommiting week 1 work

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,25 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const toc_url = "json/toc.json";
-  
-    fetch(toc_url)
-      .then((response) => response.json())
-      .then((json_object) => {
-        const toc_list = document.querySelector("#toc");
-    
-        for (item of json_object.links) {
-          const li_element = document.createElement("li");
-  
-          const anchor_element = document.createElement('a');
+  const toc_url = "json/toc.json";
 
-          anchor_element.href = item.url;
+  fetch(toc_url)
+    .then((response) => response.json())
+    .then((json_object) => {
+      const toc_list = document.querySelector("#toc");
 
-          anchor_element.innerText = item.label;
+      for (item of json_object.links) {
+        const li_element = document.createElement("li");
 
-          li_element.appendChild(anchor_element);
+        const anchor_element = document.createElement("a");
 
-          toc_list.appendChild(li_element);
+        anchor_element.href = item.url;
 
-        }
-      });
-  });
-  
+        anchor_element.innerText = item.label;
+
+        li_element.appendChild(anchor_element);
+
+        toc_list.appendChild(li_element);
+      }
+    });
+});
